aa19e3fb2a16efcb09193e2b5475c73308472243
Abhishek <Abhishekkange@gmail.com>
2024-06-12 12:15:03 +0530

    initial commit

---
+++ webrtc via ec2/client1.js
@@ -0,0 +1,27 @@
+// creating a new peer connection
+const PeerConnection1 = new RTCPeerConnection(null);
+console.log("client connection made")
+
+// create a new Data channel using the connection 
+const dataChannel = PeerConnection1.createDataChannel("datachannel");
+
+//ICE certifate event listener ( for connnection 1)
+PeerConnection1.addEventListener('icecandidate', function(event){
+
+    console.log("ice candidate created");
+    console.log("Ice candidate: "+ event.candidate);
+
+    //share this ICE candidate with client 2 using websockets
+
+});
+
+// creating an Offer
+PeerConnection1.createOffer().then((desc)=>{
+
+
+    //desc gives you the SD. set as local desc for peer1 and remote for peer2
+    console.log("offer created successfully");
+    PeerConnection1.setLocalDescription(desc);
+    //send this to peer 2 via web sockets
+    
+})
